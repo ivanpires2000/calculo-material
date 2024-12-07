8d36b38652584f36c774c834d632e05beed0d14a
Ivan Pires da Silva Ferreira <ivan.pires2000@gmail.com>
2024-12-07 12:19:23 +0000

    adição do gif carregando

--- index.php
+++ index.php
@@ -131,7 +131,9 @@
                 </div>
             </div>
         </div>
-        <div id="parede-espera"></div>
+        <div id="parede-espera" class="opacity-50">
+            <img src="images/carregando.gif" alt="Carregando">
+        </div>
     </main>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
